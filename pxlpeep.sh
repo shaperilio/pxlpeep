c6c2510f9b8d7bbb4d9f63dc9502d220c84a71c2
Emilio Graff <1@emil.io>
2021-09-06 16:02:06 -0700

    Update for new Qt version

--- pxlpeep.sh
+++ pxlpeep.sh
@@ -1,2 +1,2 @@
-LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:~/Qt/6.0.3/gcc_64/lib \
-~/build-pxlpeep-Desktop_Qt_6_0_3_GCC_64bit-Release/pxlpeep $1
+LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:~/Qt/6.1.3/gcc_64/lib \
+~/build-pxlpeep-Desktop_Qt_6_1_3_GCC_64bit-Release/pxlpeep $1
